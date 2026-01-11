90fbfa74bf8ca45a44e2bb2a5bfdb304b7f56fc9
bishalsaha-official <cse.bishalsaha@gmail.com>
2026-01-11 11:59:51 +0600

    Initial project setup

---
+++ src/Layouts/RootLayouts.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from 'react-router';
+
+const RootLayouts = () => {
+    return (
+        <div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default RootLayouts;
---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            this is hero
+        </div>
+    );
+};
+
+export default Banner;
